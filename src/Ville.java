a41f6b4702e47759a22ad729f2a45aa8875580fe
LydiaSB <lydia.souriot@etu.univ-nantes.fr>
2016-10-26 16:32:46 +0200

    algo s v1

--- src/Ville.java
+++ src/Ville.java
@@ -51,4 +51,13 @@ public class Ville {
 	public Rue getRue(int i) {
 		return _rues.get(i);
 	}
+
+
+	public ArrayList<Place> getPlaces() {
+		return _places;
+	}
+	
+	public ArrayList<Rue> getRues(){
+		return _rues;
+	}
 }
